15be0d4798c8714d830e16c9ef3e57b5c6b671ec
Hugo Herter <git@hugoherter.com>
2015-04-10 10:38:01 -0400

    Refactor add list_ids()

--- diss/meta.py
+++ diss/meta.py
@@ -5,9 +5,15 @@ from .settings import METADATA_PATH
 from .utils import loads, dumps
 
 
-def list_blobs():
+def list_ids():
+    "List all ids present in the metadata."
     for id_ in os.listdir(METADATA_PATH):
-        id_ = id_.replace('.json', '')
+        yield id_.replace('.json', '')
+
+
+def list_blobs():
+    "List all blobs by their path."
+    for id_ in list_ids():
         meta = get_meta(id_)
         yield meta['info']['path']
 
@@ -20,8 +26,7 @@ def get_meta(id_):
 
 def search_meta(term):
     "Search for a term in the metadata and return the corresponding ids"
-    for id_ in os.listdir(METADATA_PATH):
-        id_ = id_.replace('.json', '')
+    for id_ in list_ids():
         meta = get_meta(id_)
         if term in dumps(meta):
             yield id_
