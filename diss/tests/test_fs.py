8214d35f7fcc795033795c9cfe0aa4528a188008
Hugo Herter <git@hugoherter.com>
2015-04-12 13:54:24 -0400

    Enhance FUSE tests

--- diss/tests/test_fs.py
+++ diss/tests/test_fs.py
@@ -8,6 +8,11 @@ from diss.fs import id_from_path, DissFilesystem
 from .testdata import ID
 
 
+@pytest.fixture
+def fs():
+    return DissFilesystem()
+
+
 def test_id_from_path():
     assert id_from_path('/blobs/SOMEID') == 'SOMEID'
     assert id_from_path('/files/hello.txt') == ID
@@ -19,13 +24,12 @@ def test_id_from_path():
         id_from_path('/files/DOES NOT EXIST')
 
 
-def test_DissFilesystem():
-    mount_path = 'mount_dissfs'
-    if not os.path.isdir(mount_path):
-        os.mkdir(mount_path)
-    fs = DissFilesystem()
-    print(fs)
-    assert fs.readdir('/', 0) == ['blobs', 'files']
+def test_readdir(fs):
+    assert fs.readdir('/', None) == ['blobs', 'files']
+    assert set(fs.readdir('/blobs', None)).issuperset([ID])
+    assert set(fs.readdir('/files', None)).issuperset(['hello.txt'])
 
+
+def test_read(fs):
     data = fs.read('/files/hello.txt', 100, 0, None)
     assert data == b"Hello world !\n\n"
