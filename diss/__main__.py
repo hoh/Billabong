63ba398a99fb8184ca092a308e767ca5caf2eeb1
Hugo Herter <git@hugoherter.com>
2015-06-08 23:38:59 +0200

    Refactor CLI to use Baker

--- diss/__main__.py
+++ diss/__main__.py
@@ -1,5 +1,9 @@
+"""Diss: Distributed Storage System
+"""
+
 import os
 import sys
+from baker import command, run
 
 try:
     from pygments import highlight
@@ -14,78 +18,84 @@ from diss.check import check_data, check_enc_data
 from diss.utils import dumps
 from diss.sync import push_blobs
 
-HELP = '''DIstributed Storage System
 
-dis ls
-dis lsblobs
-dis add $SOMEFILE
-dis get $HASH
-dis info $HASH
-dis echo $HASH
-dis search $TERM
-dis check
-'''
+def print_record(record):
+    if highlight and '--no-color' not in sys.argv:
+        print(highlight(dumps(record),
+                        JsonLexer(),
+                        TerminalFormatter()))
+    else:
+        print(dumps(record))
 
-if len(sys.argv) > 1:
 
-    if sys.argv[1] in ('ls', 'list'):
-        format_ = "{:>8} {:>8} {:>8}"
-        for r in inventory.list_records():
-            print(format_.format(r['id'][:8],
-                                 r['size'],
-                                 r['info']['filename']))
+@command
+def ls():
+    format_ = "{:>8} {:>8} {:>8}"
+    for r in inventory.list_records():
+        print(format_.format(r['id'][:8],
+                             r['size'],
+                             r['info']['filename']))
 
-    elif sys.argv[1] == 'lsblobs':
-        for i in inventory.list_record_ids():
-            print(i)
 
-    elif sys.argv[1] == 'add':
-        target = sys.argv[2]
-        meta = add_file(target)
+@command
+def blobs():
+    for i in inventory.list_record_ids():
+        print(i)
+
+
+@command
+def add(target):
+    record = add_file(target)
+    print_record(record)
+
+
+@command
+def get(id_):
+    raise NotImplemented
+
+
+@command
+def info(id_):
+    meta = inventory.get_record(id_)
+
+    if highlight and '--no-color' not in sys.argv:
+        print(highlight(dumps(meta),
+                        JsonLexer(),
+                        TerminalFormatter()))
+    else:
         print(dumps(meta))
 
-    elif sys.argv[1] == 'get':
-        target = sys.argv[2]
-        raise NotImplemented
 
-    elif sys.argv[1] == 'info':
-        id_ = sys.argv[2]
-        meta = inventory.get_record(id_)
+@command
+def echo(id_):
+    data = get_content(id_)
 
-        if highlight and '--no-color' not in sys.argv:
-            print(highlight(dumps(meta),
-                            JsonLexer(),
-                            TerminalFormatter()))
-        else:
-            print(dumps(meta))
-
-    elif sys.argv[1] == 'echo':
-        id_ = sys.argv[2]
-        data = get_content(id_)
-
-        # Write bytes to stdout:
-        fp = os.fdopen(sys.stdout.fileno(), 'wb')
-        for chunk in data:
-            fp.write(chunk)
-            fp.flush()
-
-    elif sys.argv[1] == 'search':
-        term = sys.argv[2]
-        for i in inventory.search(term):
-            print(i)
-
-    elif sys.argv[1] == 'check':
-        # Check the validity of all blobs and metadata
-        for i in inventory.list_record_ids():
-            check_data(i)
-            check_enc_data(i)
-
-    elif sys.argv[1] == 'push':
-        # Push blobs to sync storage
-        push_blobs()
-    else:
-        print('Unknown command')
+    # Write bytes to stdout:
+    fp = os.fdopen(sys.stdout.fileno(), 'wb')
+    for chunk in data:
+        fp.write(chunk)
+        fp.flush()
 
 
-else:
-    print(HELP)
+@command
+def search(term):
+    for i in inventory.search(term):
+        print(i)
+
+
+@command
+def check():
+    "Check the validity of all blobs and metadata"
+    for i in inventory.list_record_ids():
+        check_data(i)
+        check_enc_data(i)
+
+
+@command
+def push():
+    "Push blobs to sync storage"
+    push_blobs()
+
+
+if __name__ == '__main__':
+    run()
