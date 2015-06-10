8c98fa85f6b3456477cf08680032841183b80d13
Hugo Herter <github.dev@hugoh.org>
2015-06-10 21:58:35 +0200

    Add License file and headers

--- billabong/__main__.py
+++ billabong/__main__.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2015 "Hugo Herter http://hugoherter.com"
+#
+# This file is part of Billabong.
+#
+# Intercom is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 # Billabong
 
@@ -16,7 +34,7 @@ try:
 except ImportError:
     highlight = None
 
-from diss import add_file, get_content
+from billabong import add_file, get_content
 from billabong.settings import inventory
 from billabong.check import check_data, check_enc_data
 from billabong.utils import dumps
