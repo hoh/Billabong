76c4870397cf297eb90c8522cbbf52d7473e864d
Hugo Herter <git@hugoherter.com>
2015-07-25 02:20:43 +0200

    Increase minor version number to 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 from setuptools import setup
 
 setup(name='Billabong',
-      version='0.2.0',
+      version='0.2.1',
       description='Personal Encrypted file storage, backup and sharing',
       long_description=open('README.rst').read(),
       author='Hugo Herter',
