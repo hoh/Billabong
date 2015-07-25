e5722230d1b822ba2bbeff33392aca4bf20cf844
Hugo Herter <git@hugoherter.com>
2015-07-25 12:17:33 +0200

    Increase minor version number to 0.2.3

--- billabong/__init__.py
+++ billabong/__init__.py
@@ -26,7 +26,7 @@ from .settings import inventory, stores
 from .core import Billabong
 
 __author__ = "Hugo Herter http://hugoherter.com"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 billabong = Billabong(inventory, stores)
